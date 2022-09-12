92692439dba4947d0dc3925122fa93d602e0ae4d
Quentin Renard <contact@asticode.com>
2022-09-12 11:37:30 +0200

    Added missing server tests

--- pkg/server_test.go
+++ pkg/server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -119,4 +120,166 @@ func TestServerHandler(t *testing.T) {
 
 	// Assert
 	require.Equal(t, ctxValue, <-chanCtxValue)
+}
+
+type readResult struct {
+	b   []byte
+	err error
+}
+
+func TestServerReadWriteStats(t *testing.T) {
+	// Quiet logs
+	SetLogHandler(func(l LogLevel, file, area, msg string, line int) {})
+	defer SetLogHandler(nil)
+
+	// Create server
+	chanRead := make(chan readResult)
+	srv, err := NewServer(ServerOptions{
+		Handler: ServerHandlerFunc(func(c *Connection) {
+			b := make([]byte, 1500)
+			n, err := c.Read(b)
+			if err != nil {
+				chanRead <- readResult{err: err}
+			} else {
+				chanRead <- readResult{b: b[:n]}
+			}
+			<-chanRead
+		}),
+		Host: "127.0.0.1",
+		Port: 4000,
+	})
+	require.NoError(t, err)
+	chanListenAndServe := make(chan error)
+	defer func() {
+		srv.Close()
+		<-chanListenAndServe
+	}()
+
+	// Listen and serve
+	go func() { chanListenAndServe <- srv.ListenAndServe(1) }()
+
+	// Dial
+	c, err := Dial(DialOptions{
+		Host: "127.0.0.1",
+		Port: 4000,
+	})
+	require.NoError(t, err)
+	defer c.Close()
+
+	// Write
+	msg := "test message"
+	n, err := c.Write([]byte(msg))
+	require.NoError(t, err)
+	require.Equal(t, 12, n)
+
+	// Wait for read
+	rr := <-chanRead
+
+	// Assert
+	require.NoError(t, rr.err)
+	require.Equal(t, msg, string(rr.b))
+
+	// Get stats
+	s, err := c.Stats(true, false)
+	require.NoError(t, err)
+	require.True(t, s.ByteSent() > 0)
+	require.True(t, s.ByteSentTotal() > 0)
+	s, err = c.Stats(true, false)
+	require.NoError(t, err)
+	require.False(t, s.ByteSent() > 0)
+
+	// Release read
+	chanRead <- readResult{}
+}
+
+func TestServerShutdownSuccess(t *testing.T) {
+	// Quiet logs
+	SetLogHandler(func(l LogLevel, file, area, msg string, line int) {})
+	defer SetLogHandler(nil)
+
+	// Create server
+	var handled bool
+	chanHandle := make(chan bool)
+	srv, err := NewServer(ServerOptions{
+		Handler: ServerHandlerFunc(func(c *Connection) { handled = <-chanHandle }),
+		Host:    "127.0.0.1",
+		Port:    4000,
+	})
+	require.NoError(t, err)
+	chanListenAndServe := make(chan error)
+	defer func() {
+		srv.Close()
+		<-chanListenAndServe
+	}()
+
+	// Listen and serve
+	go func() { chanListenAndServe <- srv.ListenAndServe(1) }()
+
+	// Dial
+	c, err := Dial(DialOptions{
+		Host: "127.0.0.1",
+		Port: 4000,
+	})
+	require.NoError(t, err)
+	defer c.Close()
+
+	// Shutdown
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		chanHandle <- true
+	}()
+	err = srv.Shutdown(context.Background())
+	require.NoError(t, err)
+	require.True(t, handled)
+}
+
+func TestServerShutdownDeadlineExceeded(t *testing.T) {
+	// Quiet logs
+	SetLogHandler(func(l LogLevel, file, area, msg string, line int) {})
+	defer SetLogHandler(nil)
+
+	// Create server
+	chanHandle := make(chan bool)
+	srv, err := NewServer(ServerOptions{
+		Handler: ServerHandlerFunc(func(c *Connection) { <-chanHandle }),
+		Host:    "127.0.0.1",
+		Port:    4000,
+	})
+	require.NoError(t, err)
+	chanListenAndServe := make(chan error)
+	defer func() {
+		srv.Close()
+		select {
+		case <-chanListenAndServe:
+		default:
+		}
+	}()
+
+	// Listen and serve
+	go func() {
+		chanListenAndServe <- srv.ListenAndServe(1)
+		close(chanListenAndServe)
+	}()
+
+	// Dial
+	c, err := Dial(DialOptions{
+		Host: "127.0.0.1",
+		Port: 4000,
+	})
+	require.NoError(t, err)
+	defer c.Close()
+
+	// Shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	err = srv.Shutdown(ctx)
+	require.ErrorIs(t, err, context.DeadlineExceeded)
+	err = <-chanListenAndServe
+	require.ErrorIs(t, err, ErrServerClosed)
+
+	// Second ListenAndServe should fail
+	require.ErrorIs(t, srv.ListenAndServe(1), ErrServerClosed)
+
+	// Cancel handle
+	chanHandle <- true
 }
